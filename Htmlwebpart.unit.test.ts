75c26d799d67ac6ff01d470cac3a57ce5bd60f54
Ajay Bhat <ajaybhat777@gmail.com>
2023-02-28 12:18:12 +0530

    Update Htmlwebpart.unit.test.ts

--- Htmlwebpart.unit.test.ts
+++ Htmlwebpart.unit.test.ts
@@ -1,15 +1,39 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
-import ScriptEditorWebPart from './ScriptEditorWebPart';
+import * as ReactDom from 'react-dom';
+import { Version, DisplayMode } from '@microsoft/sp-core-library';
+import ScriptEditorWebPart from '../ScriptEditorWebPart';
 
-test('renders script editor web part', () => {
-  const props = {
-    script: '<div>Hello world</div>',
-    title: 'Test Title',
-    removePadding: true,
-    spPageContextInfo: false,
-    teamsContext: false
-  };
-  const { container } = render(<ScriptEditorWebPart {...props} />);
-  expect(container.innerHTML).toContain(props.script);
+describe('ScriptEditorWebPart', () => {
+  let container: HTMLElement;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  it('renders the web part', () => {
+    const component = <ScriptEditorWebPart />;
+    ReactDom.render(component, container);
+
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+
+  it('displays the script when in read mode', () => {
+    const component = <ScriptEditorWebPart properties={{ script: '<div>Test script</div>', displayMode: DisplayMode.Read }} />;
+    ReactDom.render(component, container);
+
+    expect(container.innerHTML).toContain('<div>Test script</div>');
+  });
+
+  it('displays the script editor when in edit mode', () => {
+    const component = <ScriptEditorWebPart properties={{ script: '<div>Test script</div>', displayMode: DisplayMode.Edit }} />;
+    ReactDom.render(component, container);
+
+    expect(container.innerHTML).toContain('Edit HTML Code');
+  });
 });
