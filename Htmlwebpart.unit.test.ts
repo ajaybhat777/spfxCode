6c6e85d32fbb6534cb2c64ffb84ca176d88283bf
Ajay Bhat <ajaybhat777@gmail.com>
2023-02-28 14:58:49 +0530

    Update Htmlwebpart.unit.test.ts

--- Htmlwebpart.unit.test.ts
+++ Htmlwebpart.unit.test.ts
@@ -1,83 +1,90 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as ReactDom from 'react-dom';
 import { DisplayMode } from '@microsoft/sp-core-library';
-import { SPComponentLoader } from '@microsoft/sp-loader';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { IPropertyPaneField } from '@microsoft/sp-property-pane';
-import PropertyPaneLogo from './PropertyPaneLogo';
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IPropertyPaneConfiguration, IPropertyPaneField } from "@microsoft/sp-property-pane";
+import { IScriptEditorWebPartProps } from './IScriptEditorWebPartProps';
 import ScriptEditorWebPart from './ScriptEditorWebPart';
-import ScriptEditor from './components/ScriptEditor';
+import PropertyPaneLogo from './PropertyPaneLogo';
 
-jest.mock('react');
-jest.mock('react-dom');
-jest.mock('@microsoft/sp-core-library');
-jest.mock('@microsoft/sp-loader');
-jest.mock('@microsoft/sp-webpart-base');
-jest.mock('@microsoft/sp-property-pane');
-jest.mock('./PropertyPaneLogo');
-jest.mock('./components/ScriptEditor');
+// Mocked SPComponentLoader
+jest.mock('@microsoft/sp-loader', () => ({
+  SPComponentLoader: {
+    loadScript: jest.fn(),
+  },
+}));
+
+// Mock BaseClientSideWebPart
+class MockBaseClientSideWebPart<P> extends BaseClientSideWebPart<P> {
+  protected onInit(): Promise<void> {
+    return Promise.resolve();
+  }
+}
 
 describe('ScriptEditorWebPart', () => {
-  let scriptEditorWebPart: ScriptEditorWebPart;
-  let context: any;
+  let webPart: ScriptEditorWebPart;
+  let webPartElem: HTMLElement;
+  const renderEditorSpy = jest.spyOn(ScriptEditorWebPart.prototype, 'renderEditor');
 
   beforeEach(() => {
-    context = {
-      instanceId: 'instanceId',
-      propertyPane: {
-        register: jest.fn(),
-        refresh: jest.fn(),
-      },
+    // Mock the web part properties
+    const properties: IScriptEditorWebPartProps = {
+      script: '',
+      title: '',
+      removePadding: false,
     };
-    scriptEditorWebPart = new ScriptEditorWebPart();
-    scriptEditorWebPart.displayMode = DisplayMode.Read;
-    scriptEditorWebPart.domElement = document.createElement('div');
-    scriptEditorWebPart.context = context;
+
+    // Create a new instance of the web part
+    webPart = new ScriptEditorWebPart();
+    webPart.displayMode = DisplayMode.Read;
+    webPart.properties = properties;
+    webPart.render();
+    
+    // Mock the context
+    const context = {
+      instanceId: 'instanceId',
+      pageContext: {
+        web: {
+          absoluteUrl: '',
+        },
+      },
+      webPartProperties: properties,
+      webPartDisplayMode: DisplayMode.Read,
+      webPartContext: {
+        serviceScope: {
+          consume: jest.fn(),
+        },
+      } as any,
+      propertyPane: {
+        refresh: jest.fn(),
+      } as any,
+    };
+    
+    webPart.context = context;
+
+    // Create a new DOM element to render the web part
+    webPartElem = document.createElement('div');
+    document.body.appendChild(webPartElem);
+    webPart.render();
+
+    // Reset the spy on renderEditor
+    renderEditorSpy.mockClear();
   });
 
   afterEach(() => {
-    jest.resetAllMocks();
+    ReactDom.unmountComponentAtNode(webPartElem);
+    document.body.removeChild(webPartElem);
   });
 
-  describe('render', () => {
-    it('should render editor when displayMode is Edit', async () => {
-      scriptEditorWebPart.displayMode = DisplayMode.Edit;
-      await scriptEditorWebPart.render();
-      expect(ReactDOM.render).toHaveBeenCalledTimes(1);
-      expect(ReactDOM.render).toHaveBeenCalledWith(
-        expect.any(Object),
-        scriptEditorWebPart.domElement
-      );
-    });
+  it('renders in read mode', () => {
+    expect(renderEditorSpy).not.toHaveBeenCalled();
+    expect(webPartElem.innerHTML).toContain('');
+  });
 
-    it('should render script when displayMode is Read', () => {
-      scriptEditorWebPart.properties = {
-        script: '<div>Script Content</div>',
-        removePadding: false,
-        title: 'Script Editor Web Part',
-      };
-      scriptEditorWebPart.render();
-      expect(ReactDOM.unmountComponentAtNode).toHaveBeenCalledTimes(1);
-      expect(scriptEditorWebPart.domElement.innerHTML).toBe(
-        scriptEditorWebPart.properties.script
-      );
-    });
+  it('renders in edit mode', async () => {
+    webPart.displayMode = DisplayMode.Edit;
+    webPart.render();
 
-    it('should remove padding from parent element when removePadding is true', () => {
-      const parentElement = document.createElement('div');
-      const childElement = document.createElement('div');
-      parentElement.appendChild(childElement);
-      scriptEditorWebPart.domElement = parentElement;
-      scriptEditorWebPart.properties = {
-        script: '<div>Script Content</div>',
-        removePadding: true,
-        title: 'Script Editor Web Part',
-      };
-      scriptEditorWebPart.render();
-      expect(parentElement.style.paddingTop).toBe('0px');
-      expect(parentElement.style.paddingBottom).toBe('0px');
-      expect(parentElement.style.marginTop).toBe('0px');
-      expect(parentElement.style.marginBottom).toBe('0px');
-    });
+    expect(renderEditorSpy).toHaveBeenCalled();
   });
 });
