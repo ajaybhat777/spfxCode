a0dccc76f51bc8b594a3657a89e1064c4803f623
Ajay Bhat <ajaybhat777@gmail.com>
2023-02-28 15:34:24 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -44,4 +44,31 @@ describe('ScriptEditorWebPart', () => {
   it('renders the script in read mode', () => {
     // Arrange
     const spyUnmount = jest.spyOn(ReactDOM, 'unmountComponentAtNode');
-    const spyRender = jest.spyOn
+    const spyRender = jest.spyOn(ScriptEditorWebPart.prototype, 'render');
+    const mockProps = {
+    displayMode: DisplayMode.Read,
+    removePadding: true,
+    script: '<div id="test">Test script</div>',
+    title: 'Test title'
+    };
+    const webPart = new ScriptEditorWebPart();
+    webPart.render();
+    webPart.properties = mockProps;
+    // Act
+    webPart.render();
+
+    // Assert
+    const element = container.querySelector('#test');
+    expect(spyUnmount).toHaveBeenCalledTimes(1);
+    expect(spyRender).toHaveBeenCalledTimes(2);
+    expect(element).toBeTruthy();
+    expect(element.textContent).toBe('Test script');
+    expect(element.tagName).toBe('DIV');
+    expect(container.style.paddingTop).toBe('0px');
+    expect(container.style.paddingBottom).toBe('0px');
+    expect(container.style.marginTop).toBe('0px');
+    expect(container.style.marginBottom).toBe('0px');
+    });
+
+
+
