0f08a130179eadbb72fa9e85c07d2a4977a080ab
Ajay Bhat <ajaybhat777@gmail.com>
2023-02-28 13:03:43 +0530

    Update Htmlwebpart.unit.test.ts

--- Htmlwebpart.unit.test.ts
+++ Htmlwebpart.unit.test.ts
@@ -1,61 +1,83 @@
 import * as React from 'react';
-import * as ReactDom from 'react-dom';
+import * as ReactDOM from 'react-dom';
+import { DisplayMode } from '@microsoft/sp-core-library';
+import { SPComponentLoader } from '@microsoft/sp-loader';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { IPropertyPaneField } from '@microsoft/sp-property-pane';
+import PropertyPaneLogo from './PropertyPaneLogo';
 import ScriptEditorWebPart from './ScriptEditorWebPart';
+import ScriptEditor from './components/ScriptEditor';
 
-// Mock the SPComponentLoader and SPPropertyPane modules
-jest.mock('@microsoft/sp-component-base', () => ({
-  ...jest.requireActual('@microsoft/sp-component-base'),
-  SPComponentLoader: {
-    loadScript: jest.fn(),
-    loadCss: jest.fn(),
-  },
-}));
-jest.mock('@microsoft/sp-property-pane', () => ({
-  ...jest.requireActual('@microsoft/sp-property-pane'),
-}));
+jest.mock('react');
+jest.mock('react-dom');
+jest.mock('@microsoft/sp-core-library');
+jest.mock('@microsoft/sp-loader');
+jest.mock('@microsoft/sp-webpart-base');
+jest.mock('@microsoft/sp-property-pane');
+jest.mock('./PropertyPaneLogo');
+jest.mock('./components/ScriptEditor');
 
 describe('ScriptEditorWebPart', () => {
-  let component: ScriptEditorWebPart;
+  let scriptEditorWebPart: ScriptEditorWebPart;
+  let context: any;
 
   beforeEach(() => {
-    component = new ScriptEditorWebPart();
+    context = {
+      instanceId: 'instanceId',
+      propertyPane: {
+        register: jest.fn(),
+        refresh: jest.fn(),
+      },
+    };
+    scriptEditorWebPart = new ScriptEditorWebPart();
+    scriptEditorWebPart.displayMode = DisplayMode.Read;
+    scriptEditorWebPart.domElement = document.createElement('div');
+    scriptEditorWebPart.context = context;
   });
 
-  it('should render the editor', async () => {
-    // Mock the ReactDom.render method
-    ReactDom.render = jest.fn();
-
-    // Set the properties of the component
-    component.properties = {
-      script: 'console.log("Hello, World!");',
-      title: 'Script Editor',
-      removePadding: true,
-    };
-    component.displayMode = 1;
-
-    // Call the render method
-    component.render();
-
-    // Expect that the ReactDom.render method was called with the correct parameters
-    expect(ReactDom.render).toHaveBeenCalledWith(
-      expect.anything(),
-      component.domElement,
-    );
+  afterEach(() => {
+    jest.resetAllMocks();
   });
 
-  it('should update the script property', () => {
-    // Set the properties of the component
-    component.properties = {
-      script: 'console.log("Hello, World!");',
-      title: 'Script Editor',
-      removePadding: true,
-    };
+  describe('render', () => {
+    it('should render editor when displayMode is Edit', async () => {
+      scriptEditorWebPart.displayMode = DisplayMode.Edit;
+      await scriptEditorWebPart.render();
+      expect(ReactDOM.render).toHaveBeenCalledTimes(1);
+      expect(ReactDOM.render).toHaveBeenCalledWith(
+        expect.any(Object),
+        scriptEditorWebPart.domElement
+      );
+    });
 
-    // Call the scriptUpdate method
-    component.scriptUpdate('script', 'console.log("Hello, World!");', 'console.log("Hello, Jest!");');
+    it('should render script when displayMode is Read', () => {
+      scriptEditorWebPart.properties = {
+        script: '<div>Script Content</div>',
+        removePadding: false,
+        title: 'Script Editor Web Part',
+      };
+      scriptEditorWebPart.render();
+      expect(ReactDOM.unmountComponentAtNode).toHaveBeenCalledTimes(1);
+      expect(scriptEditorWebPart.domElement.innerHTML).toBe(
+        scriptEditorWebPart.properties.script
+      );
+    });
 
-    // Expect that the script property was updated
-    expect(component.properties.script).toBe('console.log("Hello, Jest!");');
+    it('should remove padding from parent element when removePadding is true', () => {
+      const parentElement = document.createElement('div');
+      const childElement = document.createElement('div');
+      parentElement.appendChild(childElement);
+      scriptEditorWebPart.domElement = parentElement;
+      scriptEditorWebPart.properties = {
+        script: '<div>Script Content</div>',
+        removePadding: true,
+        title: 'Script Editor Web Part',
+      };
+      scriptEditorWebPart.render();
+      expect(parentElement.style.paddingTop).toBe('0px');
+      expect(parentElement.style.paddingBottom).toBe('0px');
+      expect(parentElement.style.marginTop).toBe('0px');
+      expect(parentElement.style.marginBottom).toBe('0px');
+    });
   });
 });
