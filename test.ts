80ac42a0870f2f7dc3b529eadeb053a4c54a74c8
Ajay Bhat <ajaybhat777@gmail.com>
2023-03-01 18:58:29 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,105 +1,33 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { DisplayMode } from '@microsoft/sp-core-library';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import ScriptEditorWebPart from './ScriptEditorWebPart';
+public render(): void {
+  const element: React.ReactElement<IExampleWebPartProps> = React.createElement(
+    ContentAggregator,
+    {
+      propertyValue: this._propertyValue,
+    }
+  );
 
-// Mock the SPComponentLoader module
-jest.mock('@microsoft/sp-loader', () => ({
-  SPComponentLoader: {
-    loadScript: jest.fn(),
-  },
-}));
+  const container = this.domElement.querySelector('#contentAggregatorContainer');
 
-// Mock the ReactDom.render function
-jest.mock('react-dom', () => ({
-  render: jest.fn(),
-  unmountComponentAtNode: jest.fn(),
-}));
+  // Remove any existing content from the container element
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 
-// Define some mock web part properties
-const mockWebPartProps = {
-  script: 'console.log("Hello, world!");',
-  title: 'My Script Editor',
-  removePadding: true,
-};
+  // Render the updated element in the container element
+  ReactDOM.render(element, container);
+}
 
-// Define some mock web part context values
-const mockContext = {
-  instanceId: '12345678-1234-1234-1234-1234567890ab',
-  propertyPane: {
-    refresh: jest.fn(),
-  },
-};
+protected onInit(): Promise<void> {
+  return super.onInit().then(() => {
+    this.context.statusRenderer.displayLoadingIndicator(this.domElement, 'Loading...');
+    this._propertyValue = this.properties.description;
+    this.render();
 
-// Define some helper functions for creating instances of the ScriptEditorWebPart class
-const createWebPart = (props: any = {}, context: any = {}) => {
-  return new ScriptEditorWebPart({
-    displayMode: DisplayMode.Edit,
-    title: 'Test Web Part',
-    updateProperty: jest.fn(),
-    ...props,
-  }, {
-    ...mockContext,
-    ...context,
+    // Add the container element if it doesn't exist yet
+    if (!this.domElement.querySelector('#contentAggregatorContainer')) {
+      const container = document.createElement('div');
+      container.setAttribute('id', 'contentAggregatorContainer');
+      this.domElement.appendChild(container);
+    }
   });
-};
-
-describe('ScriptEditorWebPart', () => {
-  beforeEach(() => {
-    // Reset the mocks before each test
-    jest.clearAllMocks();
-  });
-
-  describe('render', () => {
-    it('should render the editor pane in Edit mode', async () => {
-      const webPart = createWebPart(mockWebPartProps);
-
-      await webPart.render();
-
-      expect(ReactDom.render).toHaveBeenCalled();
-      expect(ReactDom.render.mock.calls[0][0].type.name).toBe('ScriptEditor');
-    });
-
-    it('should unmount the editor pane and render the script in Read mode', async () => {
-      const webPart = createWebPart({
-        ...mockWebPartProps,
-        displayMode: DisplayMode.Read,
-      });
-
-      webPart.domElement.innerHTML = '<div>Test Script</div>';
-
-      await webPart.render();
-
-      expect(ReactDom.unmountComponentAtNode).toHaveBeenCalledWith(webPart.domElement);
-      expect(webPart.domElement.innerHTML).toBe(mockWebPartProps.script);
-    });
-
-    it('should remove padding from parent elements in Read mode when removePadding is true', async () => {
-      const webPart = createWebPart({
-        ...mockWebPartProps,
-        displayMode: DisplayMode.Read,
-      });
-
-      // Mock the parent element to have padding
-      const parentElement = document.createElement('div');
-      parentElement.style.paddingTop = '10px';
-      parentElement.style.paddingBottom = '10px';
-      webPart.domElement.parentElement = parentElement;
-
-      await webPart.render();
-
-      // Check that the padding was removed from the parent element
-      expect(parentElement.style.paddingTop).toBe('0px');
-      expect(parentElement.style.paddingBottom).toBe('0px');
-    });
-
-    it('should not remove padding from parent elements in Read mode when removePadding is false', async () => {
-      const webPart = createWebPart({
-        ...mockWebPartProps,
-        displayMode: DisplayMode.Read,
-        removePadding: false,
-      });
-
-      // Mock the parent element to have padding
-      const parentElement = document.createElement('
+}
